6802e195821c0eae2c383224611f015f56dbecbe
mahmoud ahmed Mohamed's Lap <om20031129@gmail.com>
2024-02-08 12:06:46 +0200

    added Course13

---
+++ Course13/Course_13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "clsDblLinkedList.h"
+using namespace std;
+
+
+
+int main()
+{
+
+
+}
---
+++ Course13/clsDblLinkedList.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+
+template <class T>
+class clasDblLinkedList {
+
+private:
+	T _value;
+	clsDblLinkedList *_next;
+	clsDblLinkedList* _previous;
+
+public:
+	void InsertAtBeginning(T n)	
+	{
+		
+	}
+
+
+
+	void Print()
+	{
+	}
+
+};
